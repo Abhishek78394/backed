64db1e15934af36c9e914db122cfba378bcbc57e
abhishek joshi <107804782+Abhishek78394@users.noreply.github.com>
2023-08-12 15:17:45 +0530

    new

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import User from "./routers/User.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+export const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles: true,
+  })
+);
+app.use(cors());
+
+app.use("/api/v1", User);
+
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
+
+
+import { config } from "dotenv";
+import cloudinary from "cloudinary";
+import { connectDB } from "./config/database.js";
+
+config({
+  path: "./config/.env",
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
+});
